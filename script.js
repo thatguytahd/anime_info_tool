0b5db74e8a4243d453bb280bd3d54fb00eb5651b
thatguytahd <todd.baker90@gmail.com>
2021-02-24 16:42:08 -0500

    Commented out Oatuh Authentication function as most public GET endpoints do not need authentication. Started working on first getAnimeByName function.

--- script.js
+++ script.js
@@ -6,33 +6,50 @@ To Do List:
 
 */
 
-let accessToken;
-const clientID = '';
-const clientSecret = '';
-const authURL = 'https://kitsu.io/api/oauth/token';
+// let accessToken;
+// const clientID = '';
+// const clientSecret = '';
+// const authURL = 'https://kitsu.io/api/oauth/token';
 
-let getAccessToken = (url, client_ID, client_Secret) => {
-    let key;
+// let getAccessToken = (url, client_ID, client_Secret) => {
+//     let key;
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('POST', url, true);
+//     xhr.setRequestHeader('Content-Type', 'application/json');
+//     xhr.send(`{
+//         grant_type = 'password',
+//         username: ${client_ID},
+//         password: ${client_Secret}
+//     }`);
+//     xhr.onreadystatechange = () => {
+//         if (xhr.readyState == xhr.DONE) {
+//             // const response = xhr.responseText;
+//             // let obj = JSON.parse(response);
+//             // key = obj.access_token;
+//             // accessToken = key;
+//             console.log(xhr.responseText);
+//         } else {
+//             console.log('There has been an error getting a token.');
+//         }
+//     }
+// }
+
+let getAnimeByName = (name) => {
+    let baseURL = 'https://kitsu.io/api/edge/anime/?filter[text]=';
     const xhr = new XMLHttpRequest();
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(`{
-        grant_type = password,
-        username: ${client_ID},
-        password: ${client_Secret}
-    }`);
     xhr.onreadystatechange = () => {
-        if (xhr.readyState == xhr.DONE) {
-            // const response = xhr.responseText;
-            // let obj = JSON.parse(response);
-            // key = obj.access_token;
-            // accessToken = key;
-            console.log(xhr.responseText);
+        if (xhr.status == 200) {
+            const response = xhr.responseText;
+            console.log(response);
         } else {
-            console.log('There has been an error getting a token.');
+            console.log(xhr.status);
         }
     }
+    xhr.open('GET', `${baseURL}${name}`);
+    xhr.send();
 }
 
-getAccessToken(authURL, clientID, clientSecret);
-console.log(accessToken);
+// getAccessToken(authURL, clientID, clientSecret);
+// console.log(accessToken);
+
+getAnimeByName('My Hero Academia');
