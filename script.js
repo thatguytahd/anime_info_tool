a2113a3ee8d4ebeca5b6c29b8993ed9dcaf5f46d
Todd Baker <todd.baker90@gmail.com>
2021-02-24 17:45:50 -0500

    Continuation of testing trying to display just title from API response

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* 
 To Do List:
 
--Write function to get access from Kitsu API
+-Write function to get access from Kitsu API - Found out in the documentation that most public GET endpoints does not require authentication.
 -Write XHR to pull info on a specific anime to test
 
 */
@@ -34,13 +34,31 @@ To Do List:
 //     }
 // }
 
-let getAnimeByName = (name) => {
+const updateAnimeResult = (
+    anime = {
+        canonicalTitle:''
+    },
+) => {
+    console.log(anime.canonicalTitle);
+}
+
+const parseData = (response) => {
+    try {
+        console.log(response.text);
+        return JSON.parse(response);
+    } catch(e) {
+        console.log('parseData failed.')
+    }
+}
+
+const getAnimeByName = (name) => {
     let baseURL = 'https://kitsu.io/api/edge/anime/?filter[text]=';
     const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
+    xhr.onreadystatechange = (r) => {
         if (xhr.status == 200) {
-            const response = xhr.responseText;
-            console.log(response);
+            // const response = xhr.responseText;
+            // console.log(response);
+            updateAnimeResult(parseData(r));
         } else {
             console.log(xhr.status);
         }
