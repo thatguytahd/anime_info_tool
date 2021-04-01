0a622a6613f4487eadf77318ca40d1e7157fc2a5
Todd Baker <todd.baker90@gmail.com>
2021-04-01 11:04:08 -0400

    removing some old notes from script.js

--- script.js
+++ script.js
@@ -1,38 +1,4 @@
-/* 
-To Do List:
 
--Write function to get access from Kitsu API - Found out in the documentation that most public GET endpoints does not require authentication.
--Write XHR to pull info on a specific anime to test
-
-*/
-
-// let accessToken;
-// const clientID = '';
-// const clientSecret = '';
-// const authURL = 'https://kitsu.io/api/oauth/token';
-
-// let getAccessToken = (url, client_ID, client_Secret) => {
-//     let key;
-//     const xhr = new XMLHttpRequest();
-//     xhr.open('POST', url, true);
-//     xhr.setRequestHeader('Content-Type', 'application/json');
-//     xhr.send(`{
-//         grant_type = 'password',
-//         username: ${client_ID},
-//         password: ${client_Secret}
-//     }`);
-//     xhr.onreadystatechange = () => {
-//         if (xhr.readyState == xhr.DONE) {
-//             // const response = xhr.responseText;
-//             // let obj = JSON.parse(response);
-//             // key = obj.access_token;
-//             // accessToken = key;
-//             console.log(xhr.responseText);
-//         } else {
-//             console.log('There has been an error getting a token.');
-//         }
-//     }
-// }
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
 const searchForm = document.getElementById('searchForm');
@@ -69,7 +35,7 @@ const loadSearchResults = (obj) => {
                 </section>
             `;
         } else if(anime.meta.count === 0) {
-            result.innerHTML = '<h2>Sorry! There were no results for your search</h2>';   
+            result.innerHTML = '<h2>Sorry! There were no results for your search, try again.</h2>';   
         }
         resultsList.appendChild(result);
     }
